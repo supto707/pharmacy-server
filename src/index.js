dc05fb62af7906f2e8b124328c1d7bc109b8b5ae
supto707 <kaunia.supto@gmail.com>
2025-12-31 15:56:24 +0600

    env server

--- src/index.js
+++ src/index.js
@@ -26,7 +26,8 @@ const allowedOrigins = [
   'http://localhost:5173',
   'http://localhost:8080',
   'http://127.0.0.1:5173',
-  'http://127.0.0.1:8080'
+  'http://127.0.0.1:8080',
+  'https://pharmacy-client-supto.vercel.app'
 ];
 
 // Socket.io setup
